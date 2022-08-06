6230ba2a84cde2a2d462560dfb5ab0c0899c259b
Mandeepsgm <mandeepsg.m@gmail.com>
2022-08-06 02:56:42 -0400

    final changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import Accordion from "./components/Accordion";
 import Dropdown from "./components/Dropdown";
 import Search from "./components/Search";
@@ -34,9 +34,17 @@ const options = [
 ];
 
  const App= () => {
+    const [selected, setSelected] = useState(options[0]);
+    const [showDropdown, setShowDropdown] = useState(true);
     return (
         <div>
-            <Dropdown options={options} />
+        <button onClick={()=> setShowDropdown(!showDropdown)}>Toggle Dropdown</button>
+        { showDropdown?
+            <Dropdown 
+                selected={selected}
+                onSelectedChange={setSelected}
+                options={options} 
+            /> : null}
         </div>
     );
 };
